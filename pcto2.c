e227864524eaf5a2b2c287380a773d681e5ef853
verdi4na <127745074+verdi4na@users.noreply.github.com>
2023-03-13 16:55:18 +0100

    Add files via upload

---
+++ pcto2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int y;
+
+    printf("inserisci un numero: ");
+    scanf("%d", &x);
+    printf("inserisci un altro numero: ");
+    scanf("%d", &y);
+    printf("I numeri inseriti sono x = %d e y = %d\n", x, y);
+}

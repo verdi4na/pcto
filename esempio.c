5533b25d42823fd4440e8268439f0c128b73d225
verdi4na <127745074+verdi4na@users.noreply.github.com>
2023-03-13 15:50:01 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    int numero = 42;
+    double numerolungo = 778554386543;
+    float numeroconlavirgola = 1.2;
+    char carattere = 'c';
+
+printf("il numero è %d\n", numero);
+printf("numerolungo è ;%f, numero con la virgola è: %f\n", numerolungo, numeroconlavirgola);
+printf("ïl carattere è %c\n", carattere);
+}
+  
